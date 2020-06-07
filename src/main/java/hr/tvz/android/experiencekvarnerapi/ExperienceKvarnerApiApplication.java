04c16530105e962b1ac2076854c34eeb278b143d
Neven Jakopčić <nevenjakopcic@gmail.com>
2020-06-07 22:51:53 +0200

    Initial commit

---
+++ src/main/java/hr/tvz/android/experiencekvarnerapi/ExperienceKvarnerApiApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.android.experiencekvarnerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceKvarnerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperienceKvarnerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/android/experiencekvarnerapi/ExperienceKvarnerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.android.experiencekvarnerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperienceKvarnerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

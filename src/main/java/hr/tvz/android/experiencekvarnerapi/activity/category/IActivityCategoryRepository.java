db4a9b10f9f673c9e54d48b2eebfd3ae258f5f27
Neven Jakopčić <nevenjakopcic@gmail.com>
2020-06-08 20:32:52 +0200

    Fix /api/cities/{cityID}/activity-categories GET

--- src/main/java/hr/tvz/android/experiencekvarnerapi/activity/category/IActivityCategoryRepository.java
+++ src/main/java/hr/tvz/android/experiencekvarnerapi/activity/category/IActivityCategoryRepository.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface IActivityCategoryRepository extends JpaRepository<ActivityCategory, Long> {
 
-    @Query(value = "select distinct AC.* from ACTIVITY_CATEGORY AC inner join ACTIVITY A on AC.ID = A.ACTIVITY_CATEGORY_FK inner join CITY C on A.CITY_FK = C.ID", nativeQuery = true)
+    @Query(value = "select distinct AC.* from ACTIVITY_CATEGORY AC inner join ACTIVITY A on AC.ID = A.ACTIVITY_CATEGORY_FK inner join CITY C on A.CITY_FK = C.ID where C.ID = :cityID", nativeQuery = true)
     List<ActivityCategory> findDistinctActivityCategoriesOfCity(Long cityID);
 }
